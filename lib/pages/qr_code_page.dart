6be770a76d35c68daa20d73278d2632ea6732675
Shouryan Nikam <snnikam@umich.edu>
2023-04-29 22:05:41 -0400

    add demo of qr code showing

--- lib/pages/qr_code_page.dart
+++ lib/pages/qr_code_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QrCodePage extends StatelessWidget {
   const QrCodePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("QR Code"),
+    return Scaffold(
+      body: Center(
+        child: QrImage(
+            data:
+                "https://www.hackdearborn.org", //need to fetch this from some server
+            version: QrVersions.auto,
+            size: 300.0,
+            padding: const EdgeInsets.all(10.0),
+            errorCorrectionLevel: QrErrorCorrectLevel.H,
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            gapless: true,
+            errorStateBuilder: (cxt, err) {
+              return const Center(
+                child: Text(
+                  "Uh oh! Something went wrong...",
+                  textAlign: TextAlign.center,
+                ),
+              );
+            }),
+      ),
     );
   }
 }
