372f85a81ea68bbc167efc55829ffe895ce8b3e9
khanabdu25 <khanabdu@umich.edu>
2023-05-11 16:32:56 -0400

    Filter menu functionality bug fixes
    - Provider package imported for the functionality below
    - NavBar now collapses on selection of only one view
    - Agenda view settings moved to AppState to preserve user preference on Agenda page exit
    - Details page time chips refactored
                - for events with no definite
                   end time only one chip is
                   displayed

---
+++ lib/pages/agenda_page/timechips.dart
@@ -0,0 +1,107 @@
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:hd2_app/pages/agenda_page/getDataSource.dart';
+
+class TimeChips extends StatelessWidget {
+  final DateTime from;
+  final DateTime to;
+
+  const TimeChips({Key? key, required this.from, required this.to}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (from == to) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Chip(
+            avatar: Container(
+              width: 60,
+              height: 30,
+              decoration: BoxDecoration(
+                color: Colors.purple.shade800,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text(
+                  '@',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            backgroundColor: Colors.pink.shade800,
+            label: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                DateFormat('hh:mm a').format(from).toString(),
+              ),
+            ),
+          ),
+        ],
+      );
+    } else {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Chip(
+            avatar: Container(
+              width: 60,
+              height: 30,
+              decoration: BoxDecoration(
+                color: Colors.purple.shade800,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text(
+                  'From:',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 9,
+                  ),
+                ),
+              ),
+            ),
+            backgroundColor: Colors.pink.shade800,
+            label: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                DateFormat('hh:mm a').format(from).toString(),
+              ),
+            ),
+          ),
+          SizedBox(width: 10),
+          Chip(
+            avatar: Container(
+              width: 60,
+              height: 30,
+              decoration: BoxDecoration(
+                color: Colors.purple.shade800,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text(
+                  'To:',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 9,
+                  ),
+                ),
+              ),
+            ),
+            backgroundColor: Colors.pink.shade800,
+            label: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                DateFormat('hh:mm a').format(to).toString(),
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+  }
+}
+
