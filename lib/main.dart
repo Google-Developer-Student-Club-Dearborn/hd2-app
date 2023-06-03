4ba6b47e0f4ca787b15f2af94fd1a62965c53857
rosy <rosyshrestha028@gmail.com>
2023-06-03 19:01:29 -0400

    Configure push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 // pub.dev libraries
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/services.dart';
 
 // splash screen
 import 'package:hd2_app/splash_screen.dart';
@@ -10,6 +13,7 @@ import 'package:hd2_app/splash_screen.dart';
 import 'package:hd2_app/pages/agenda_page.dart';
 import 'package:hd2_app/pages/information_page.dart';
 import 'package:hd2_app/pages/qr_code_page.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,10 +24,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HackDearborn 2',
-      theme: ThemeData(brightness: Brightness.dark),
-      home: const MyHomePage(title: 'HackDearborn 2'),
+    return OverlaySupport(
+      child: MaterialApp(
+        title: 'HackDearborn 2',
+        theme: ThemeData(brightness: Brightness.dark),
+        home: const MyHomePage(title: 'HackDearborn 2'),
+      ),
     );
   }
 }
@@ -36,52 +42,237 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _pageIndex = 0;
 
+  late int _totalNotifications;
+  PushNotification? _notificationInfo;
+  late final FirebaseMessaging _messaging;
+
+  @override
+  void initState() {
+    _totalNotifications = 0;
+
+    // For handling notification when the app is in background but not terminated
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      PushNotification notification = PushNotification(
+        title: message.notification?.title,
+        body: message.notification?.body,
+      );
+      setState(() {
+        _notificationInfo = notification;
+        _totalNotifications++;
+      });
+    });
+
+    checkForInitialMessage();
+
+    super.initState();
+  }
+
+  checkForInitialMessage() async {
+    await Firebase.initializeApp();
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      PushNotification notification = PushNotification(
+        title: initialMessage.notification?.title,
+        body: initialMessage.notification?.body,
+      );
+      setState(() {
+        _notificationInfo = notification;
+        _totalNotifications++;
+      });
+    }
+  }
+
+  void registerNotification() async {
+    await Firebase.initializeApp();
+
+    _messaging = FirebaseMessaging.instance;
+
+    // On iOS, this helps to take the user permissions
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+
+      // For handling the received notifications
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        // Parse the message received
+        PushNotification notification = PushNotification(
+          title: message.notification?.title,
+          body: message.notification?.body,
+          dataTitle: message.data['title'],
+          dataBody: message.data['body'],
+        );
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(totalNotifications: _totalNotifications),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: const Duration(seconds: 2),
+          );
+        }
+      });
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Stack(
+  //     children: [
+  //       Scaffold(
+  //         appBar: AppBar(
+  //           title: Text(widget.title),
+  //           elevation: 0,
+  //           // backgroundColor: Colors.black,
+  //         ),
+  //         body: Builder(
+  //           builder: (context) {
+  //             switch (_pageIndex) {
+  //               case 0:
+  //                 return const AgendaPage();
+  //               case 1:
+  //                 return const InformationPage();
+  //               default:
+  //                 return const QrCodePage();
+  //             }
+  //           },
+  //         ),
+  //         // NotificationBadge(totalNotifications: _totalNotifications),
+  //         bottomNavigationBar: CurvedNavigationBar(
+  //           index: _pageIndex,
+  //           backgroundColor: Theme.of(context).primaryColor,
+  //           color: Theme.of(context).primaryColorDark,
+  //           animationDuration: const Duration(milliseconds: 250),
+  //           height: 75.0,
+  //           animationCurve: Curves.easeOutSine,
+  //           items: const <Widget>[
+  //             Icon(Icons.view_agenda_sharp, size: 30),
+  //             Icon(Icons.info, size: 30),
+  //             Icon(Icons.qr_code, size: 30),
+  //           ],
+  //           onTap: (index) {
+  //             setState(() {
+  //               _pageIndex = index;
+  //             });
+  //           },
+  //         ),
+  //       ),
+  //       const SplashScreen()
+  //     ],
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
-            elevation: 0,
-            // backgroundColor: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notify'),
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'App for capturing Firebase Push Notifications',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
           ),
-          body: Builder(
-            builder: (context) {
-              switch (_pageIndex) {
-                case 0:
-                  return const AgendaPage();
-                case 1:
-                  return const InformationPage();
-                default:
-                  return const QrCodePage();
-              }
-            },
-          ),
-          bottomNavigationBar: CurvedNavigationBar(
-            index: _pageIndex,
-            backgroundColor: Theme.of(context).primaryColor,
-            color: Theme.of(context).primaryColorDark,
-            animationDuration: const Duration(milliseconds: 250),
-            height: 75.0,
-            animationCurve: Curves.easeOutSine,
-            items: const <Widget>[
-              Icon(Icons.view_agenda_sharp, size: 30),
-              Icon(Icons.info, size: 30),
-              Icon(Icons.qr_code, size: 30),
-            ],
-            onTap: (index) {
-              setState(() {
-                _pageIndex = index;
-              });
-            },
-          ),
-        ),
-        const SplashScreen()
-      ],
+          _notificationInfo != null
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'TITLE: ${_notificationInfo!.dataTitle ?? _notificationInfo!.title}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      'BODY: ${_notificationInfo!.dataBody ?? _notificationInfo!.body}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                )
+              : Container(),
+          const SizedBox(height: 16.0),
+          NotificationBadge(totalNotifications: _totalNotifications),
+          const SizedBox(height: 16.0),
+          // TODO: add the notification text here
+        ],
+      ),
     );
   }
+}
+
+class NotificationBadge extends StatelessWidget {
+  final int totalNotifications;
+
+  const NotificationBadge({required this.totalNotifications});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40.0,
+      height: 40.0,
+      decoration: const BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            '$totalNotifications',
+            style: const TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PushNotification {
+  PushNotification({
+    this.title,
+    this.body,
+    this.dataTitle,
+    this.dataBody,
+  });
+
+  String? title;
+  String? body;
+  String? dataTitle;
+  String? dataBody;
 }
