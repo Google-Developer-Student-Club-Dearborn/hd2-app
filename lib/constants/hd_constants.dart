97c6096ddc8afc45a813aead1914a81f7c447d70
rosy <rosyshrestha028@gmail.com>
2023-08-09 22:20:13 -0400

    Create constants

---
+++ lib/constants/hd_constants.dart
@@ -0,0 +1,13 @@
+class HDConstants {
+  static const int NOTIFICATION_OFFSET = 10;
+
+  static const String HOME_PAGE = '/home';
+
+  static const String EVENT_DETAILS_PAGE = '/event_details';
+
+  static const String QR_CODE_PAGE = '/qr_code';
+
+  static const int NOTIFICATION_1_ID = 50;
+
+  static const int NOTIFICATION_2_ID = 51;
+}
